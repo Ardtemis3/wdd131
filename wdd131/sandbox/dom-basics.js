3939aff3dfcefadb96dd41f11793a05934143e7e
Ken Ard III <kennyard3@byui.edu>
2025-01-15 09:18:36 -0700

    update + start of mission

--- wdd131/sandbox/dom-basics.js
+++ wdd131/sandbox/dom-basics.js
@@ -1,3 +1,12 @@
 const newParagraph = document.createElement("p");
 newParagraph.innerText = "Added with Javascript!";
 document.body.appendChild(newParagraph);
+const img = document.querySelector("img");
+img.setAttribute("src", "https://picsum.photos/200");
+img.setAttribute("alt", "200");
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
+document.body.appendChild(newDiv);
+const newSect = document.createElement("section");
+newSect.innerHTML = "<h2>DOM Basics</h2><p>This was added through Javascript<p>";
+document.body.appendChild(newSect);
