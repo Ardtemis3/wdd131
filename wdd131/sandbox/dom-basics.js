2929e3a9330ac8803f4a3dbf4861eb49b0a73f00
Ken Ard III <kennyard3@byui.edu>
2025-01-18 20:42:12 -0700

    update

--- wdd131/sandbox/dom-basics.js
+++ wdd131/sandbox/dom-basics.js
@@ -10,3 +10,35 @@ document.body.appendChild(newDiv);
 const newSect = document.createElement("section");
 newSect.innerHTML = "<h2>DOM Basics</h2><p>This was added through Javascript<p>";
 document.body.appendChild(newSect);
+
+// const newList = document.createElement("list");
+// newList.innerHTML = "olo";
+// document.body.append(newList);
+const ingredientData = document.createElement("ul");
+ingredientData.innerText = ["Pinto Beans", " Corn", " Spices", " Tortillas"];
+document.body.appendChild(ingredientData);
+
+const portionData = document.createElement("ul");
+portionData.innerText = [" 1 15oz can", " 1 15oz can", " 1 Tbsp", " 8"];
+document.body.appendChild(portionData);
+
+// other ways
+
+// function ingredientTemplate(index) {
+//     return `<li>${portionData[index]} ${ingredientData[index]}</li>`
+// }
+
+// const newList = document.createElement("ul");
+// ingredientData.forEach(function(item, index) {
+//     newList.innerHTML += ingredientTemplate(index);
+// })
+// document.body.append(newList);
+
+// newList.classList.add("dark")
+
+// const newList = document.createElement("ul");
+// newList.innerHtml = `
+// <li>${portionData[0]} ${ingredientData[0]}</li>
+// <li>${portionData[1]} ${ingredientData[1]}</li>
+// <li>${portionData[2]} ${ingredientData[2]}</li>
+// <li>${portionData[3]} ${ingredientData[3]}</li>`;
