3093abeb862089b9c37fc550823575801d58ea41
Ken Ard III <kennyard3@byui.edu>
2025-01-18 22:31:01 -0700

    update

--- wdd131/mission/index.js
+++ wdd131/mission/index.js
@@ -7,19 +7,14 @@ img.setAttribute("src", "byui-logo_white.png");
 const styleSelector = document.getElementByClass('styleSelector');
 const body = document.body;
 
-// Function to change the body's class
 function changeBodyClass(event) {
-  // Get the selected value
   const selectedStyle = event.target.value;
 
-  // Remove all possible classes from the body
   body.className = '';
 
-  // Add the class corresponding to the selected style
   body.classList.add(selectedStyle);
 }
 
-// Attach event listener to the select element
 styleSelectors.forEach(selector => {
     selector.addEventListener('change', changeBodyClass);
   });
